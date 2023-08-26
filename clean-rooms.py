f0dd7e193b75b48eba32e39ef6e0da03b37a2acc
Maximilian Heinzler <m.heinzler@heinzler.de>
2023-08-26 20:41:11 +0200

    Use hashed passwords in config files

--- clean-rooms.py
+++ clean-rooms.py
@@ -1,6 +1,7 @@
 #! python3
 
 import dreame
+import mock
 import yaml
 
 # create a device from the config file
@@ -10,9 +11,17 @@ with open('config.yaml', 'r') as file:
 device = dreame.DreameVacuumDevice(**config)
 
 try:
-    # connect to the device locally and the through the cloud
+    # connect to the device locally
     device.connect_device()
-    device.connect_cloud()
+
+    # connect to the cloud while mocking MD5 hashing to allow using hashes
+    # instead of plain text passwords in the config
+    @mock.patch('hashlib.md5')
+    def connect_cloud_mock_md5(md5_mock):
+        md5_mock.return_value.hexdigest.return_value = config['password']
+        device.connect_cloud()
+
+    connect_cloud_mock_md5()
 finally:
     # stop the communication with the device
     device.disconnect()
