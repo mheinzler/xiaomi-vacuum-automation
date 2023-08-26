9ff0466ba5c0f6992edd517fcd16672e0db0ea35
Maximilian Heinzler <m.heinzler@heinzler.de>
2023-08-27 00:24:42 +0200

    Fix dreame module not being found when run outside the project directory

--- dreame.py
+++ dreame.py
@@ -1,11 +1,17 @@
 import importlib.util
+import os
 import sys
+
+# we need to construct the module path relative to the project directory
+project_dir = os.path.dirname(__file__)
+dreame_path = (
+    'dreame_vacuum/custom_components/dreame_vacuum/dreame/__init__.py')
 
 # manually import the dreame subdirectory to avoid all references to Home
 # Assistant
 dreame_spec = importlib.util.spec_from_file_location(
     'dreame',
-    './dreame_vacuum/custom_components/dreame_vacuum/dreame/__init__.py')
+    os.path.join(project_dir, dreame_path))
 dreame = importlib.util.module_from_spec(dreame_spec)
 
 sys.modules['dreame'] = dreame
