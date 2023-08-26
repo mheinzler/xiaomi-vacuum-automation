6d4c0d4537d472ddbc8d79647101363d4df9f983
Maximilian Heinzler <m.heinzler@heinzler.de>
2023-08-26 22:20:29 +0200

    Wait for map

--- clean-rooms.py
+++ clean-rooms.py
@@ -4,13 +4,19 @@ import argparse
 import dreame
 import logging
 import mock
+import time
 import yaml
+
+SLEEP_INTERVAL = 0.25
 
 # parse the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', metavar='FILE',
                     default='config.yaml',
                     help='device configuration file (default: config.yaml)')
+parser.add_argument('-t', '--timeout', metavar='SECONDS', type=int,
+                    default=10,
+                    help='timeout for map retrieval (default: 10 seconds)')
 
 parser.add_argument('-v', '--verbose',
                     default=logging.WARNING,
@@ -45,6 +51,24 @@ try:
         device.connect_cloud()
 
     connect_cloud_mock_md5()
+
+    # wait for the map to be available
+    logging.info('Waiting for map ...')
+    status = device.status
+    for i in range(int(args.timeout / SLEEP_INTERVAL)):
+        if status.current_map:
+            break
+
+        time.sleep(SLEEP_INTERVAL)
+
+    if status.current_map:
+        logging.info('Received map from device')
+
+        segments = status.segments
+        logging.info('Rooms: %s',
+                     [[id, seg.name] for id, seg in segments.items()])
+    else:
+        logging.error('Timeout while waiting for map')
 finally:
     # stop the communication with the device
     device.disconnect()
